536aafa40fc05c7e28eb3c486e4fae1f99d9626b
James Johnson <jimmy.l.johnson@gmail.com>
2016-01-24 18:14:49 -0600

    Adopted GPLv3

--- viewer.py
+++ viewer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #
 # Viewer code
-#    Copyright (C) {year}  {name of author}
+#    Copyright (C) 2016 James Johnson
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
